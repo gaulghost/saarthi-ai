845e09a1e7a62f8e6a5249c637db14da2b6ca2d5
pradhuman <pradhuman.singh120201@gmail.com>
2021-01-25 17:14:07 +0530

    Correcting overlapping in form

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Label from './Label.js'
+// import Label from './Label.js'
 
 import { ValidatorForm, TextValidator,SelectValidator} from 'react-material-ui-form-validator';
 import { Button} from '@material-ui/core';
@@ -36,13 +36,13 @@ const Form = (props) => {
     }
 
     return(
-        <div class="container-contact100">
+        <div className="container-contact100">
             <button className="contact100-form-btn closeform-btn" onClick={props.closeForm}>
                 <span>
                     Close Form
                 </span>
             </button>
-    		<div class="wrap-contact100">
+    		<div className="wrap-contact100">
                 <div className="contact100-form-title">
                     <span className="contact100-form-title-1">
                         New Employee Form
@@ -54,7 +54,7 @@ const Form = (props) => {
                 </div>
                 <ValidatorForm
                 onSubmit={props.clickedSubmit}
-                onError={errors => console.log(errors)}
+
             >
                 <TextValidator
                     style={styleLabels}
@@ -84,9 +84,11 @@ const Form = (props) => {
                     errorMessages={["this field is required"]}
                     className=""
                  >
-                   <MenuItem value={10}>Ten</MenuItem>
-                   <MenuItem value={20}>Twenty</MenuItem>
-                   <MenuItem value={30}>Thirty</MenuItem>
+                   <MenuItem value={'Economics'}>Economics</MenuItem>
+                   <MenuItem value={'Accounts'}>Accounts</MenuItem>
+                   <MenuItem value={'Development'}>Development</MenuItem>
+                   <MenuItem value={'Buisness'}>Buisness</MenuItem>
+                   <MenuItem value={'Services'}>Services</MenuItem>
                 </SelectValidator>
                 <TextValidator
                     type="email"
@@ -99,8 +101,10 @@ const Form = (props) => {
                     errorMessages={['this field is required']}
                 />
                 <TextValidator
+                    onFocus={(e)=>{e.target.type = 'date'}}
                     style={styleLabels}
-                    type="date"
+                    onBlur={(e)=>{e.target.type = 'text'}}
+                    type="text"
                     label="Date of Joining"
                     onChange={props.setValue}
                     name="doj"
@@ -113,39 +117,6 @@ const Form = (props) => {
                 <Button style={buttonStyle} onClick = {props.clickedClear}>Clear</Button>
                 </div>
             </ValidatorForm>
-                 {/*<form className="contact100-form validate-form">
-                    <Label name='name' content="Name" type="text" placeholder="Full Name" value={props.formData.name} onChange={props.setValue}/>
-                    <Label name='emp_id' content="Employee ID" type="text" placeholder="Alloted ID" value={props.formData.emp_id} onChange={props.setValue}/>
-                    <div className="wrap-input100x validate-input" data-validate = "Select your Department">
-        				<div className="label-input100">Department</div>
-        				<div>
-        					<select className="js-select2" name='dept' value={props.formData.dept} onChange={props.setValue}>
-        						<option>Choose one</option>
-        						<option>UI/UX Design</option>
-        						<option>eCommerce Bussiness</option>
-        						<option>Online Services</option>
-        					</select>
-        					<div className="dropDownSelect2"></div>
-        				</div>
-        				<span className="focus-input100"></span>
-        			</div>
-                    <Label name='email' content="Email ID" type="email" placeholder="Registered Email ID" value={props.formData.email} onChange={props.setValue}/>
-                    <Label name='doj' content="Date of Joining" type="date" placeholder="Joining Date" value={props.formData.doj} onChange={props.setValue}/>
-                    <div className="container-contact100-form-btn">
-                        <button className="contact100-form-btn" onClick={(e)=>{props.clickedSubmit(e)}}>
-                            <span>
-                                Submit
-                                <i className="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
-                            </span>
-                        </button>
-                        <button className="contact100-form-btn" onClick={props.clickedClear}>
-                            <span>
-                                Clear
-                                <i className="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
-                            </span>
-                        </button>
-                    </div>
-                </form>*/}
             </div>
         </div>
     )
